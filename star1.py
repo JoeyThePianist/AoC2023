1ed5a72a11814d7a523fe5bd36081cdb555b47c5
JoeyThePianist <24552347+JoeyThePianist@users.noreply.github.com>
2023-12-02 02:33:50 -0700

    Completed First Star

--- star1.py
+++ star1.py
@@ -1,1 +1,26 @@
-print("Hello World")
+def main():
+    puzzle = open("./assets/star1puzzle.txt", "r")
+    puzzle_content = puzzle.read()
+    lines = puzzle_content.splitlines()
+    total = 0
+    for line in lines:
+        result = parseLine(line)
+        total = total + result
+    print(total)
+    
+def parseLine(line):
+    first_number = None
+    last_number = None
+    for character in line:
+        if character.isdigit():
+            first_number = character
+            break
+    for index in range(len(line) -1, -1, -1):
+        character = line[index]
+        if character.isdigit():
+            last_number = character
+            break
+    return int(first_number + last_number)
+
+if __name__ == "__main__":
+    main()
